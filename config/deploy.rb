f91ada3e22ae424f3c4fad5186ca00297e1b0107
stecochrane <stevie@yoyo.local>
2015-02-27 21:55:34 +0000

    Corrected deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,8 +21,7 @@ role :db, "192.168.1.99", :primary => true
 set :keep_releases, 5
 
 # files we want symlinking to specific entries in shared.
-set :linked_files, %w{config/database.yml}
-set :linked_files, %w{config/config.yml}
+set :linked_files, %w{config/database.yml config/config.yml}
 
 # dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system lib/assets}
